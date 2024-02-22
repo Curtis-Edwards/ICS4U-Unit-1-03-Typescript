e208ddc57ebfc11d38f80988fd77927b668815ae
Curtis Edwards <curtis-edwards>
2024-02-22 14:45:36 -0500

    Added code

--- index.ts
+++ index.ts
@@ -8,10 +8,15 @@
  */
 
 import { createPrompt } from 'bun-promptx'
-// constants
+// constants & variables
 const subCookTime = 60
 const pizzaCookTime = 45
 const soupCookTime = 105
+let cookTimeMinutes
+let cookTimeSeconds
+let totalCookTime
+let lunchAmount
+let lunchAmountInt
 
 // input
 const lunchType = createPrompt("Are you heating sub, pizza, or soup?(lowercase): ")
@@ -20,44 +25,44 @@ const lunchTypeString = lunchType.value
 // process
 switch (lunchTypeString) {
   case `sub`:
-    const lunchAmount = createPrompt("How many are you heating?(Max 3): ")
-    const lunchAmountInt = parseInt(lunchAmount.value || "-1")
+    lunchAmount = createPrompt("How many are you heating?(Max 3): ")
+    lunchAmountInt = parseInt(lunchAmount.value || "-1")
     // error check
-    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0) {
+    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0 || lunchAmountInt > 3) {
       console.log("Invalid input.")
     } else {
       // process & output
-      const totalCookTime = (subCookTime / 2) + ((subCookTime / 2) * lunchAmountInt)
-      const cookTimeSeconds = totalCookTime % 60
-      const cookTimeMinutes = Math.floor(totalCookTime / 60)
+      totalCookTime = (subCookTime / 2) + ((subCookTime / 2) * lunchAmountInt)
+      cookTimeSeconds = totalCookTime % 60
+      cookTimeMinutes = Math.floor(totalCookTime / 60)
       console.log(`Total cook time:\n${cookTimeMinutes} minutes, ${cookTimeSeconds} seconds.`)
     }
     break
   case `pizza`:
-    const lunchAmount = createPrompt("How many are you heating?(Max 3): ")
-    const lunchAmountInt = parseInt(lunchAmount.value || "-1")
+    lunchAmount = createPrompt("How many are you heating?(Max 3): ")
+    lunchAmountInt = parseInt(lunchAmount.value || "-1")
     // error check
-    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0) {
+    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0 || lunchAmountInt > 3) {
       console.log("Invalid input.")
     } else {
       // process & output
-      const totalCookTime = (subCookTime / 2) + ((pizzaCookTime / 2) * lunchAmountInt)
-      const cookTimeSeconds = totalCookTime % 60
-      const cookTimeMinutes = Math.floor(totalCookTime / 60)
+      totalCookTime = (subCookTime / 2) + ((pizzaCookTime / 2) * lunchAmountInt)
+      cookTimeSeconds = totalCookTime % 60
+      cookTimeMinutes = Math.floor(totalCookTime / 60)
       console.log(`Total cook time:\n${cookTimeMinutes} minutes, ${cookTimeSeconds} seconds.`)
     }
     break
   case `soup`:
-    const lunchAmount = createPrompt("How many are you heating?(Max 3): ")
-    const lunchAmountInt = parseInt(lunchAmount.value || "-1")
+    lunchAmount = createPrompt("How many are you heating?(Max 3): ")
+    lunchAmountInt = parseInt(lunchAmount.value || "-1")
     // error check
-    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0) {
+    if (isNaN(lunchAmountInt) == true || lunchAmountInt < 0 || lunchAmountInt > 3) {
       console.log("Invalid input.")
     } else {
       // process & output
-      const totalCookTime = (subCookTime / 2) + ((soupCookTime / 2) * lunchAmountInt)
-      const cookTimeSeconds = totalCookTime % 60
-      const cookTimeMinutes = Math.floor(totalCookTime / 60)
+      totalCookTime = (subCookTime / 2) + ((soupCookTime / 2) * lunchAmountInt)
+      cookTimeSeconds = totalCookTime % 60
+      cookTimeMinutes = Math.floor(totalCookTime / 60)
       console.log(`Total cook time:\n${cookTimeMinutes} minutes, ${cookTimeSeconds} seconds.`)
     }
     break
